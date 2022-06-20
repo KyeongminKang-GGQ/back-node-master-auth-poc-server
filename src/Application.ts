b3bf16300e8ec4166a992071bd4dc767c50b595d
KyeongminKang-GGQ <kyeongmin.kang@ggq.gg>
2022-06-20 17:28:06 +0900

    Add body-parser dependency

--- src/Application.ts
+++ src/Application.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import { Routes } from "./routes/Routes";
+import bodyParser from "body-parser";
 
 export class Application {
     public express: express.Application;
@@ -10,12 +11,18 @@ export class Application {
         this.express = express();
     }
 
+    private config(): void {
+        this.express.use(bodyParser.json());
+        this.express.use(bodyParser.urlencoded({ extended: true }));
+    }
+
     private setRoutes(): void {
         this.routes = new Routes(this.alias);
         this.routes.routes(this.express);
     }
 
     public initialize() {
+        this.config();
         this.setRoutes();
     }
 }
